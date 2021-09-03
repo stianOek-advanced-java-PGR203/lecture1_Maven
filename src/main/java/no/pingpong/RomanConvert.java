2ff191778a709fcd1deb9e0db29bd04d3a03a287
TerjeSlaatto <Terjeslaatto@gmail.com>
2021-09-03 11:11:28 +0200

    shouldConvert3 (passes) + refactor

--- src/main/java/no/pingpong/RomanConvert.java
+++ src/main/java/no/pingpong/RomanConvert.java
@@ -3,11 +3,11 @@ package no.pingpong;
 public class RomanConvert {
 
     public static String toRoman(int number) {
-       if(number == 2) {
-           return "II";
-       } else {
-           return "I";
-       }
+        String romanString = "";
+        for (int i = 0; i < number; i++) {
+            romanString += "I";
+        }
+            return romanString;
     }
 
 
