9cf70d81c1aff61c6af86a0d28ad9823e8ae0368
TerjeSlaatto <Terjeslaatto@gmail.com>
2021-09-03 11:00:14 +0200

    test shouldConvert1toI(passes)

--- src/test/java/no/pingpong/RomanConverterTest.java
+++ src/test/java/no/pingpong/RomanConverterTest.java
@@ -11,5 +11,8 @@ public class RomanConverterTest {
     void shouldConvert1ToI() {
         assertEquals("I",toRoman(1));
     }
-
+    @Test
+    void shouldConvert2ToII() {
+        assertEquals("II",toRoman(2));
+    }
 }
